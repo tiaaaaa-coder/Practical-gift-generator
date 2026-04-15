c5207e82c07daacee676ae8ce1d7d2bca3c508db
halit-elselmani <khaledgalal9877@gmail.com>
2026-04-15 19:20:47 +0300

    Refactor messages for multilingual support
    Updated gift messages to support multiple languages.

--- script.js
+++ script.js
@@ -280,12 +280,19 @@ const UIFactory = {
 const badge = this.createElement('span', 'occasion-badge', messages[occasion] || messages.general);
 const name = this.createElement('strong', 'gift-name', gift);
 
-const extraMessages = {
-  birthday: '🎉 Make their day special!',
-  graduation: '🎓 Celebrate their success!',
-  holiday: '🎄 Spread joy and happiness!',
-  general: '✨ A perfect choice for any time!'
-};
+const messages = currentLanguage === "ar"
+  ? {
+      birthday: '🎂 هدية مثالية لعيد الميلاد: ',
+      graduation: '🎓 هدية رائعة للتخرج: ',
+      holiday: '🎄 فكرة هدية للعطلة: ',
+      general: '🎁 هدية مقترحة: '
+    }
+  : {
+      birthday: '🎂 Perfect birthday gift: ',
+      graduation: '🎓 Great graduation gift: ',
+      holiday: '🎄 Holiday gift idea: ',
+      general: '🎁 Recommended gift: '
+    };
 
 const extra = this.createElement('p', 'extra-message', extraMessages[occasion] || extraMessages.general);
 
